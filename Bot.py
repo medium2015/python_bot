04f6cb5e7ec898e1b4a365401dc1ad71e8ed2bff
medium2015 <medium2015@gmail.com>
2017-06-12 21:57:57 +0300

    Create Bot.py

--- Bot.py
+++ Bot.py
@@ -14,44 +14,6 @@ time1 ={'сколько время':1,'который час':2,'время':3,'
 bot = telebot.TeleBot(config.token)
 
 
-WEBHOOK_HOST = '109.86.1.91'
-WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '109.86.1.91'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-WEBHOOK_SSL_CERT = './SSL.cer' # Путь к сертификату
-WEBHOOK_SSL_PRIV = './Keys_Pr.pem'  # Путь к приватному ключу
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
-
-# Указываем настройки сервера CherryPy
-cherrypy.config.update({
-    'server.socket_host': WEBHOOK_LISTEN,
-    'server.socket_port': WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': WEBHOOK_SSL_PRIV
-})
-
-
-
-
-# Наш вебхук-сервер
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-        
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
     print ("User: %s says: %s" % (message.chat.id, message.text)) 
@@ -77,8 +39,5 @@ def repeat_all_messages(message):
     else:
         bot.send_message(message.chat.id, message.text) 
         
-#Собственно, запуск!
-cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
-
-"""if __name__ == '__main__':
-    bot.polling(none_stop=True)"""
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
